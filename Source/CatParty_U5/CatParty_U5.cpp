e780cf8eb1094cade4274949e2e300329c29d434
Victor Manuel Villegas Escobar <victor.villegas@tutanota.com>
2021-07-10 17:27:44 -0500

    Delete Source directory

--- Source/CatParty_U5/CatParty_U5.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "CatParty_U5.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CatParty_U5, "CatParty_U5" );
